2a48dcdb92fdaccbf156116f62d00085c7371cc2
Flames Co. Robotics <113800788+FlamesCoLTDRobotics@users.noreply.github.com>
2022-12-09 17:19:27 -0800

    [Request to upload 12.9.20XX]

---
+++ [HelloWorld12.9.20XX].ts
@@ -0,0 +1,9 @@
+// Define a function to print the message
+function sayHello() {
+    // Use the console.log function to print the message
+    console.log("Hello World");
+  }
+  
+  // Call the sayHello function to print the message
+  sayHello();
+  
